933ccd4881a34f8757a5d368b587b3815dc344bf
Vishal Rana <isvishalrana@gmail.com>
2024-08-21 23:33:16 +0530

    feat: add authentication

---
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+import { Loader2 } from "lucide-react";
+import { SignUp, ClerkLoaded, ClerkLoading } from "@clerk/nextjs";
+
+export default function SingUpPage() {
+  return (
+    <div className="min-h-screen grid grid-cols-1 lg:grid-cols-2">
+      <div className="h-full lg:flex flex-col items-center justify-center px-4">
+        <div className="text-center space-y-4 pt-16">
+          <h1 className="font-bold text-3xl text-[#2E2A47]">Welcome Back</h1>
+          <p className="text-base text-#[738CA0]">
+            Login in or Create account to get back to your dashboard{" "}
+          </p>
+        </div>
+        <div className="flex-cen mt-8">
+          <ClerkLoaded>
+            <SignUp />
+          </ClerkLoaded>
+          <ClerkLoading>
+            <Loader2 className="animate-spin text-muted-foreground" />
+          </ClerkLoading>
+        </div>
+      </div>
+      <div className="h-full bg-blue-600 hidden lg:flex items-center justify-center">
+        <Image src="/logo.svg" alt="logo" height={100} width={100} />
+      </div>
+    </div>
+  );
+}
