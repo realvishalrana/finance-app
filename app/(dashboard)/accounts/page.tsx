0785966c1d0941692290a7fb6c565bdb03608030
Vishal Rana <isvishalrana@gmail.com>
2024-09-21 13:31:13 +0530

    setup table component with sorting, delete, filter and pagination functionality

---
+++ app/(dashboard)/accounts/page.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
+import { Plus } from "lucide-react";
+import { columns, Payment } from "./columns";
+import { DataTable } from "@/components/data-table";
+
+const data: Payment[] = [];
+
+const AccountPage = () => {
+  const newAccout = useNewAccount();
+
+  return (
+    <div className="max-w-screen-2xl mx-auto pb-10 -mt-24">
+      <Card className="border-none drop-shadow-sm ">
+        <CardHeader className="gap-y-2 lg:flex-row lg:items-center lg:justify-between">
+          <CardTitle className="text-xl line-clamp-1">Account Page</CardTitle>
+          <Button onClick={newAccout.onOpen} className="sm">
+            <Plus className="size-4 mr-2" />
+            Add new
+          </Button>
+        </CardHeader>
+        <CardContent>
+          <DataTable
+            columns={columns}
+            data={data}
+            filterKey="email"
+            onDelete={() => {}}
+            disabled={false}
+          />
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default AccountPage;
